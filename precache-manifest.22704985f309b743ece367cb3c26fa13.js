5f2e62e7a5ed4a926a9893fc652b0a3b58703253
MirEnovi <garciaivonne11@gmail.com>
2018-10-21 04:48:34 -0500

    Updates

---
+++ precache-manifest.22704985f309b743ece367cb3c26fa13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "cd7e02e58b4cf0b60c23",
+    "url": "/react-gh-pages/static/js/main.cd7e02e5.chunk.js"
+  },
+  {
+    "revision": "2e13c2295ebdccc06da4",
+    "url": "/react-gh-pages/static/js/1.2e13c229.chunk.js"
+  },
+  {
+    "revision": "cd7e02e58b4cf0b60c23",
+    "url": "/react-gh-pages/static/css/main.4c798901.chunk.css"
+  },
+  {
+    "revision": "dce305915ee7527435bf8d9ed277d1d0",
+    "url": "/react-gh-pages/index.html"
+  }
+];
