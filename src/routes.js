5e0056d54a8ed5d903e2a97869586bf7c61dc012
MirEnovi <garciaivonne11@gmail.com>
2018-10-22 18:39:54 -0500

    fix bug in deploy 5

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import { Route, Switch,	BrowserRouter } from 'react-router-dom';
 import App from './components/app/App'; // Componente Principal
 import Login from './components/login';
 import Menu from './components/menu'; 
-// import Page404 from './components/page404';
+import Page404 from './components/page404';
 
 const AppRoutes = () => {
 	return (
@@ -14,8 +14,8 @@ const AppRoutes = () => {
 		<BrowserRouter basename = { process.env.PUBLIC_URL }>
 			<Switch>
 				<Route path = '/' component = { Login } exact />
-				<Route path = '/menu' component = { Menu } exact />
-				{/* <Route component = { Page404 } /> */}
+				<Route path = '/menu' component = { Menu }  />
+				<Route component = { Page404 } />
 			</Switch>
 		</BrowserRouter>
 	</App>
