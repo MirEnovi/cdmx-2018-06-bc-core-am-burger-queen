34b550ab4113f677c0f7e7c5b1fb3cbd49f9d93c
MirEnovi <garciaivonne11@gmail.com>
2018-10-25 03:26:08 -0500

    Updates

---
+++ precache-manifest.223b8f609dc682503cba6efed1bd38e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "563bde506f8203df569970eddccac2dd",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/burguerB3.563bde50.png"
+  },
+  {
+    "revision": "2436c630200ce0ad6c0b24f01b6048d5",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/burguer2.2436c630.png"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "cee4a85224974b978422",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.cee4a852.chunk.js"
+  },
+  {
+    "revision": "a92758389581eb253a0e",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.a9275838.chunk.js"
+  },
+  {
+    "revision": "cee4a85224974b978422",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.289b0c43.chunk.css"
+  },
+  {
+    "revision": "130de16f39d74696a94a0a9d7aeefb49",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
