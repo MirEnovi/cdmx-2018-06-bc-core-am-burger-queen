c2fa66993b24e7f347ccb2a4d2b9810a4e3039c8
MirEnovi <garciaivonne11@gmail.com>
2018-11-06 03:41:19 -0600

    change url start in manifest

--- src/components/global/nav/nav.js
+++ src/components/global/nav/nav.js
@@ -13,21 +13,6 @@ const Nav = (props) => {
 
 	return (
 		<header >
-			{/* <nav  className='row nav-header'>
-				<div className='col s10 offset-s1 nav-margin'>
-					<img src={logo} className='nav-logo col s2 m1' alt='logo burger' />
-
-					<h2 className= 'font-nav font-title col s5'>Burger <span className='font-queen'>Queen</span></h2>
-
-					<div className=''>
-						<a onClick={props.desayuno} className='truncate waves-effect waves-light btn cyan darken-4'>Desayuno</a>
-						<a onClick={props.almuerzo} className='truncate waves-effect waves-light btn deep-orange accent-3'>Almuerzo y Cena</a>
-						<a onClick={offSesion} className=' yellow darken-4 waves-effect waves-light btn '>
-							<Link to = '/'>Salir</Link>	
-						</a>
-					</div>
-				</div>
-			</nav> */}
 			<nav  className='row nav-header'>
 				<div className='col s12 l10 offset-l1 nav-margin'>
 
